4b189d591558489f30504dac52526f745c0ed8e7
RicardoP <picoricardo3d@outlook.com>
2023-11-18 01:20:25 -0500

    Made the UI for the header book form and the book item

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Book {
     constructor(title, author, pages, isRead = false) {
         this.title = title
         this.author = author
-        this.pages = pages
+        this.pages = pages;
         this.isRead = isRead;
     }
 }
