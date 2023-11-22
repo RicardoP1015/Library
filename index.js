08195442b8d737584921970ee374319091bd28a3
RicardoP <picoricardo3d@outlook.com>
2023-11-22 05:51:37 -0500

    Made a functions that create book icons and push them on to the grid
    the first function creates book icon from what the user inputs in the
    new book form
    
    the second function updates the gird by clearing the innerHTML
    then the third function pushes the newly created book to the grid

--- index.js
+++ index.js
@@ -1,52 +1,97 @@
 const submitNewBook = document.getElementById('book-submit');
+const libraryGrid = document.querySelector('.library-grid');
 
 class Library {
     constructor() {
-        this.books = []
-    }
+        this.books = [];
+    };
 
     addBook(newBook) {
         if (!this.isInLibrary(newBook)) {
-            this.books.push(newBook)
-        }
-    }
+            this.books.push(newBook);
+        };
+    };
 
     removeBook(title) {
-        this.books = this.books.filter((book) => book.title !== title)
-    }
+        this.books = this.books.filter((book) => book.title !== title);
+    };
 
     getBook(title) {
-        return this.books.find((book) => book.title === title)
-    }
+        return this.books.find((book) => book.title === title);
+    };
 
     isInLibrary(newBook) {
-        return this.books.some((book) => book.title === newBook.title)
-    }
+        return this.books.some((book) => book.title === newBook.title);
+    };
 }
 
-const library = new Library()
+const library = new Library();
 
 class Book {
     constructor(title, author, pages, isRead = false) {
-        this.title = title
-        this.author = author
+        this.title = title;
+        this.author = author;
         this.pages = pages;
         this.isRead = isRead;
+    };
+};
+
+const getBookInfo = () => {
+    const title = document.getElementById('book-name').value;
+    const author = document.getElementById('author-name').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('isRead').checked;
+    return new Book(title, author, pages, isRead);
+};
+
+const addToLibrary = () => {
+    updateLibraryGrid();
+    for (let book of library.books) {
+        createBook(book);
     }
 }
 
-const getBookInfo = () => {
-    const title = document.getElementById('book-name').value
-    const author = document.getElementById('author-name').value
-    const pages = document.getElementById('pages').value
-    const isRead = document.getElementById('isRead').checked
-    return new Book(title, author, pages, isRead)
+const updateLibraryGrid = () =>{
+    libraryGrid.innerHTML = '';
+}
+
+const createBook = (book) => {
+    const bookItem = document.createElement('div');
+    const bookMark = document.createElement('div');
+    const titleWrapper = document.createElement('div');
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+
+    bookItem.classList.add('book');
+    bookMark.classList.add('book-mark');
+    titleWrapper.classList.add('title-wrapper');
+    title.classList.add('title');
+    author.classList.add('author');
+    pages.classList.add('pages');
+
+    title.textContent = book.title;
+    author.textContent = `by ${book.author}`;
+    pages.textContent = `${book.pages} pages`;
+
+
+    if (book.isRead) {
+        bookMark.classList.add('book-read');
+    }
+
+    bookItem.appendChild(pages);
+    titleWrapper.appendChild(title);
+    titleWrapper.appendChild(author);
+    bookItem.appendChild(titleWrapper);
+    bookItem.appendChild(bookMark);
+    libraryGrid.appendChild(bookItem);
 }
 
 const addBook = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newBook = getBookInfo();
     library.addBook(newBook);
-}
+    addToLibrary();
+};
 
-submitNewBook.addEventListener('click', addBook)
+submitNewBook.addEventListener('click', addBook);
